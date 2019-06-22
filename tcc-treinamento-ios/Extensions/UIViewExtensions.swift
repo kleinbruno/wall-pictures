d58f8e8b41e325fed02ea37c93daeb215186985c
Luan Winck <luan.winck@cwi.com.br>
2019-06-22 09:50:47 -0300

    adding structure

---
+++ tcc-treinamento-ios/Extensions/UIViewExtensions.swift
@@ -0,0 +1,39 @@
+//
+//  UIViewExtensions.swift
+//  tcc-treinamento-ios
+//
+//  Created by Luan Winck on 22/06/19.
+//  Copyright © 2019 CWI software. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var borderColor: UIColor? {
+        get { return layer.borderColor.map(UIColor.init) }
+        set { layer.borderColor = newValue?.cgColor }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get { return layer.borderWidth }
+        set { layer.borderWidth = newValue }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get { return layer.cornerRadius }
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+    }
+    
+    func fixInView(_ container: UIView!) -> Void{
+        self.translatesAutoresizingMaskIntoConstraints = false;
+        self.frame = container.frame;
+        container.addSubview(self);
+        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+    }
+}
