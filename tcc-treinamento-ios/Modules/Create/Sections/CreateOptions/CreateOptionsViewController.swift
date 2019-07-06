13ee8842aa458e077ae74f3df2292532c3001a5f
Bruno A. Klein <brunokl23@hotmail.com>
2019-07-06 00:38:32 -0300

    creating screen

---
+++ tcc-treinamento-ios/Modules/Create/Sections/CreateOptions/CreateOptionsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CreateOptionsViewController.swift
+//  tcc-treinamento-ios
+//
+//  Created by Bruno Klein on 05/07/19.
+//  Copyright © 2019 CWI software. All rights reserved.
+//
+
+import UIKit
+
+class CreateOptionsViewController: UIViewController {
+    var pictures: [Picture] = []
+    
+//    let viewController = TryOutViewController.instantiate(fromAppStoryboard: .TryOut)
+//
+//    var selectedPictures: [Picture] = []
+//
+//    if let selectedItems = allPicturesCollectionView.indexPathsForSelectedItems {
+//
+//        for indexPath in selectedItems {
+//            selectedPictures.append(self.allPicturesPresenter.pictureList[indexPath.row])
+//        }
+//    }
+//
+//    viewController.pictures = selectedPictures
+//
+//    self.navigationController?.show(viewController, sender: sender)
+}
